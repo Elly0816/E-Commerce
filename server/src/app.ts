1eb4733eb8327091579c371cea75b6515a9d5924
Eleazar Udo <elzoremmanuel@gmail.com>
2023-02-27 18:48:53 +0200

    initial commit

---
+++ server/src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+
+
+dotenv.config({path: `${path.join(__dirname, '../../.env')}`});
+
+const app = express();
+app.use(cors());
+
+// console.log(path.join(__dirname, '../../.env'));
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`server running on ${process.env.PORT}`);
+});
