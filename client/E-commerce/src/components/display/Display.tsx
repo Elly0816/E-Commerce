eb0733e9cb2398dd962beaf07b84be24609253d1
Eleazar Udo <elzoremmanuel@gmail.com>
2023-03-02 21:18:36 +0200

    added image for display component

--- client/E-commerce/src/components/display/Display.tsx
+++ client/E-commerce/src/components/display/Display.tsx
@@ -5,7 +5,7 @@ export default function Display(){
 
 
     return <section className="display">
-        <img src="https://as1.ftcdn.net/v2/jpg/02/71/77/56/1000_F_271775672_yo8ZgraN2IHGbfqP2k0PsLjwvmatUNUJ.jpg"
+        <img src="./images/shopping.jpg"
          alt="shopping img" />
          <Button text="Learn More" link=""/>
     </section>
